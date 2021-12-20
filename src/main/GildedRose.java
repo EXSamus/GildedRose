699c9288498714b6a5423a7834dfc488147a8f7e
EXSamus <55688566+EXSamus@users.noreply.github.com>
2021-12-20 13:40:46 +0100

    refactoring 1

--- src/main/GildedRose.java
+++ src/main/GildedRose.java
@@ -2,61 +2,127 @@ package main;
 
 class GildedRose {
     Item[] items;
-
+    
+    String[] specialItemName = {"Aged Brie",
+    		"Backstage passes to a TAFKAL80ETC concert",
+    		"Sulfuras, Hand of Ragnaros"};
+    
     public GildedRose(Item[] items) {
         this.items = items;
     }
+    
+  //---------------------------
+    /**
+     *
+     * Vérifie si l'item est un objet "Conjured"
+     * 
+     * @param	String	nom de l'item
+     * @return	boolean	true si l'item est "Conjured" sinon false
+     * 
+     * */
+    public boolean conjuredCheck(String item) {
+		String[] s = item.split(" ");
+		
+		for(int i = 0; i<s.length; i++) {
+			if(s[i].equals("Conjured")) {
+				return true;
+			}
+		}
+		return false;
+    }
+  //---------------------------
+    /**
+    *
+    * Vérifie si l'item est un objet fait partie des objets spéciaux
+    * 
+    * @param	String	nom de l'item
+    * @return	boolean	true si l'item est spécial sinon false
+    * 
+    * */
+    public boolean specialItemCheck(Item item) {
+    	for (int i = 0; i < specialItemName.length; i++) {
+    		if(item.name.equals(specialItemName[i])) {
+    			return true;
+    		}
+    	}
+    	return false;
+    }
+  //---------------------------  
+    /**
+    *
+    * Mets à jour la qualité d'un objet spécial
+    * 
+    * @param	Item	l'objet Item
+    * 
+    * */
+    public void specialItemUpdate(Item item) {
+    	if (item.quality < 50) {
+            item.quality ++;
+    	}
+    	if(item.name.equals("Backstage passes to a TAFKAL80ETC concert" )) {
+    		if (item.sellIn < 11) { // Si il ne reste que 10 jours
+                if (item.quality < 50) {
+                    item.quality ++;
+                }
+            }    
+            if (item.sellIn < 6) { // Si il ne reste que 5 jours
+                if (item.quality < 50) {
+                    item.quality ++;
+                }
+            }
+    	}
+    }
+  //---------------------------   
+    /**
+    *
+    * Mets à jour la qualité d'un objet normal ou "Conjured"
+    * 
+    * @param	Item	l'objet Item
+    * 
+    * */
+    public void normalItemUpdate(Item item) {
+    	if(conjuredCheck(item.name)) {
+    		item.quality = item.quality - 2;
+    	} else if(item.quality>0){
+    		item.quality --;
+    	}   	
+    }
+  //---------------------------
 
+    public void sellInUpdate(Item item) {
+    	if(!item.name.equals("Sulfuras, Hand of Ragnaros") ) {
+    		item.sellIn --;
+    	} else {
+    		item.sellIn = 0;
+    	}
+    }
+    
+//---------------------------
+    
+    public void afterSellInCheck(Item item) {
+    	if (item.sellIn <= 0 && item.name.equals("Backstage passes to a TAFKAL80ETC concert" )){
+    			item.quality = 0;
+    		}	
+    	
+    }
+  //---------------------------  
+    
     public void updateQuality() {
+    	// début de la boucle
+    	// déplacement dans le tableau d'Item
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
+            if (specialItemCheck(items[i])){
+                
+            	specialItemUpdate(items[i]);
+            	  	
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
+            	normalItemUpdate(items[i]);
             }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
+            
+            sellInUpdate(items[i]);
+            
+            afterSellInCheck(items[i]);
         }
+     
     }
 }
